df2da8364dcf7fd3dc1f04da81463f479af61de8
Felipe Di Bernardi S Thiago <felipe.dibernardi@gmail.com>
2016-10-27 16:07:02 -0200

    Adicao de plugin de Checkstyle

--- target/generated-sources/annotations/br/com/fdbst/restexample/entity/Usuario_.java
+++ target/generated-sources/annotations/br/com/fdbst/restexample/entity/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-27T10:24:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-27T16:02:33")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
