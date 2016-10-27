b635a3d110dbbfba2049394553a823ad95b12ecc
Felipe Di Bernardi S Thiago <felipe.dibernardi@gmail.com>
2016-10-27 17:01:52 -0200

    Adicao de um teste

---
+++ src/test/java/br/com/fdbst/restexample/entity/UsuarioTest.java
@@ -0,0 +1,25 @@
+package br.com.fdbst.restexample.entity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+
+/**
+ *
+ * @author felipe
+ */
+public class UsuarioTest {
+    
+    public UsuarioTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() {}
+}
